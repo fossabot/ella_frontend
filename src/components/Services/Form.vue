1ceacef7646c2895d1cbae8c1a8aa0fbf89d0ce8
Julian Pollinger <neferin42@gmail.com>
2020-11-09 16:20:55 +0100

    Json-Form

--- src/components/Services/Form.vue
+++ src/components/Services/Form.vue
@@ -1,13 +1,15 @@
 <template>
-  <p>Form</p>
+  <json-form :json="service.form"></json-form>
 </template>
 
 <script>
+import jsonForm from "@educorvi/vue-json-form"
 import serviceMixin from "@/components/Services/serviceMixin";
 
 export default {
   name: "Form",
-  mixins: [serviceMixin]
+  mixins: [serviceMixin],
+  components: {jsonForm}
 }
 </script>
 
