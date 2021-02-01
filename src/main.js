5718b676608c59cc414367bb2f457a14889e2dab
Julian Pollinger <julian.pollinger@jp-studios.de>
2021-02-01 10:44:45 +0100

    Vue Material für Wizard included

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ Vue.use(IconsPlugin)
 import Vue2TouchEvents from "vue2-touch-events";
 Vue.use(Vue2TouchEvents);
 
+import {MdSteppers, MdButton} from "vue-material/dist/components"
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+Vue.use(MdSteppers);
+Vue.use(MdButton);
+
 import './registerServiceWorker'
 
 import router from './router'
